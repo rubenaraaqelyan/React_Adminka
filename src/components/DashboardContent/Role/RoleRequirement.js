39f4971f9e61853add21f4990891beca1b0b9b45
Ruben Araqelyan <ruben.araqelyan.1993@mail.ru>
2021-06-17 15:09:13 +0400

    Adminka

---
+++ src/components/DashboardContent/Role/RoleRequirement.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react'
+import edit from "../../../img/edit.svg";
+import deleteI from "../../../img/delete.svg";
+import {useDispatch, useSelector} from "react-redux";
+import {filtredRole} from "../../../redux/reducers/roleReducer";
+import {showOrgMenu} from "../../../redux/reducers/organizationReducer";
+import {showPopupAlertAction} from "../../../redux/reducers/dashboardReducer";
+import {PopupAlert} from "../../PopupAlert/PopupAlert";
+
+export const RoleRequirement = () => {
+    const {role, filterRole} = useSelector(({roleReducer}) => roleReducer)
+    const [activeItem, setActiveItem] = useState(null)
+    const [data, setData] = useState([])
+    const dispatch = useDispatch()
+    const [user, setUser] = useState({})
+
+    const deleteUser = (user) => {
+        setTimeout(() => dispatch(showPopupAlertAction(false)), 0)
+        const fUsers = filterRole.filter(i => i.require !== user.require)
+        dispatch(filtredRole(fUsers))
+    }
+    const clickDeleteIcon = (user) => {
+        setUser(user)
+        dispatch(showPopupAlertAction(true))
+    }
+
+
+    useEffect(()=>{
+        setData(filterRole)
+        dispatch(showOrgMenu(false))
+    },[filterRole])
+
+    return (
+        <>
+            <PopupAlert name={user.require} showPopup={() => deleteUser(user)}/>
+            <div className="table-users_head grid-row-3"><p>ID</p>
+                <p>Название Роли</p>
+                <p style={{textAlign: 'right'}}>Действия</p>
+                <div className="table-line"></div>
+            </div>
+            <div className="table-users_body">
+                {data && data.map(i => (
+                    <div
+                        className={`table-users_body-item grid-row-3 ${activeItem === i.id ? 'active-item-table' : ''}`}
+                        key={i.id} onClick={() => setActiveItem(i.id)}>
+                        <p>{i.userId}</p>
+                        <p>{i.require}</p>
+                        <div className="table-user_actions"><img src={edit}
+                                                                 alt="edit"/><img
+                            src={deleteI} alt="deleteI" onClick={() => clickDeleteIcon(i)}/></div>
+                        <div className="table-line"></div>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
+}
---
+++ src/components/DashboardContent/information/ResultClose/ExplainCloseResult/ExplainClose.js
@@ -0,0 +1,59 @@
+import React, {useEffect, useState} from 'react';
+import edit from '../../../../../img/edit.svg';
+import deleted from '../../../../../img/delete.svg';
+import {useDispatch, useSelector} from "react-redux";
+import _ from 'lodash'
+import {deleteFunc} from "../../../../../utils/deleteFunc";
+import {filtredCloseData} from "../../../../../redux/reducers/explainReducer";
+import {showPopupAlertAction} from "../../../../../redux/reducers/dashboardReducer";
+import {PopupAlert} from "../../../../PopupAlert/PopupAlert";
+
+
+const ExplainClose = () => {
+    const {explainClose, filterExplain} = useSelector(({typeExplainReducer}) => typeExplainReducer)
+    const dispatch = useDispatch()
+    const [activeItem, setActiveItem] = useState(null)
+    const [data, setData] = useState([])
+    const [result, setResult] = useState({})
+
+    const deleteUser = (result) => {
+        return deleteFunc(result, 'explain', filterExplain, filtredCloseData, dispatch)
+    }
+    const clickDeleteIcon = (result) => {
+        setResult(result)
+        dispatch(showPopupAlertAction(true))
+    }
+
+    useEffect(() => {
+        // dispatch(getExplainCloseData())
+        setData(filterExplain)
+    }, [filterExplain])
+    return (
+        <>
+            <PopupAlert name={result.explain} showPopup={() => deleteUser(result)}/>
+            <div className="table-users">
+                <div className="table-users_head grid-row-3"><p>ID</p>
+                    <p>Описание результата закрытия</p>
+                    <p style={{"text-align": "right"}}>Действия</p>
+                    <div className="table-line"></div>
+                </div>
+                <div className="table-users_body">
+                    {data && data.map(i => (
+                        <div
+                            className={`table-users_body-item grid-row-3 ${activeItem === i.id ? 'active-item-table' : ''}`}
+                            key={_.uniqueId()} onClick={() => setActiveItem(i.id)}>
+                            <p>{i.userId}</p>
+                            <p>{i.explain}</p>
+                            <div className="table-user_actions"><img src={edit}
+                                                                     alt="edit"/><img
+                                src={deleted} alt="deleted" onClick={() => clickDeleteIcon(i)}/></div>
+                            <div className="table-line"></div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default ExplainClose;
